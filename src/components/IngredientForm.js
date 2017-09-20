ffa858bc928b8270bc7f631827552549279e414b
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2017-09-20 17:50:06 -0400

    working on recipe form

--- src/components/IngredientForm.js
+++ src/components/IngredientForm.js
@@ -7,18 +7,61 @@ class IngredientForm extends React.Component {
 		this.state = {
 			amount: '',
 			unit: '',
-			name: ''
+			name: '',
+			submitted: false
 		}
 	}
 
+	handleChange = (event) => {
+		switch (event.target.name) {
+			case 'amount':
+				this.setState({amount: event.target.value, submitted: false});
+				break;
+			case 'unit':
+				this.setState({unit: event.target.value, submitted: false});
+				break;
+			case 'name':
+				this.setState({name: event.target.value, submitted: false});
+				break;
+			default:
+				break;
+		}
+	}
+
+	handleClick = (event) => {
+		event.preventDefault()
+	console.log("handleClick",this.state)
+		this.setState({submitted: true})
+		this.props.addIngredient(this.state)
+		
+	}
+
+	handleDelete = (event) => {
+		event.preventDefault()
+		console.log("In Handle delete",this.props)
+		this.props.removeIngredient(this.state, this.props.id)
+	}
+
 	render() {
-		return (
-			<div>
-				<input type='text' placeholder='Amount'/>
-				<input type='text' placeholder='Unit'/>
-				<input type='text' placeholder='Name'/>
-			</div>
-		)
+		if (this.state.submitted) {
+			return (
+				<div>
+					<p>{this.state.amount}</p>
+					<p>{this.state.unit}</p>
+					<p>{this.state.name}</p>
+					<button onClick={this.handleDelete}>Delete</button>
+				</div>
+			)
+		} else {
+			return (
+				<div>
+					<input name='amount' onChange={this.handleChange} type='text' placeholder='Amount'/>
+					<input name='unit' onChange={this.handleChange} type='text' placeholder='Unit'/>
+					<input name='name' onChange={this.handleChange} type='text' placeholder='Name'/>
+					<button onClick={this.handleClick}>Add</button>
+				</div>
+			)
+		}
 	}
 }
 
--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -6,20 +6,46 @@ class RecipeForm extends React.Component {
 	constructor() {
 		super()
 		this.state = {
-			ingredients: []
+			ingredientForms: [],
+			ingredientObjects: [],
+			counter: 0
 		}
 	}
 	
 	handleIngredientClick = (event) => {
 		event.preventDefault()
-		this.setState({ingredients: [...this.state.ingredients, <IngredientForm/>]})
+		this.setState({
+			ingredientForms: [...this.state.ingredientForms, <IngredientForm id={this.state.counter} addIngredient={this.addIngredient} removeIngredient={this.removeIngredient}/>],
+			counter: this.state.counter++
+		})
+	}
+
+	addIngredient = (ingredientState) => {
+		this.setState({ingredientObjects: [...this.state.ingredientObjects, ingredientState]})
+		console.log(this.state.ingredientObjects)
+	}
+
+	removeIngredient = (ingredientState, id) => {
+		console.log(ingredientState, id)
+		this.setState({
+			ingredientForms: this.state.ingredientForms.filter(form => form.id !== id),
+			ingredientObjects: this.state.ingredientObjects.filter(ingredient => ingredient !== ingredientState)
+		})
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault()
 	}
 
 	render() {
 		return (
-			<form>
+			<form onSubmit={this.handleSubmit}>
+				<input type='text' placeholder='Name' />
+				<input type='text' placeholder='Style' />
+				<input type='textarea' placeholder='Description' />
+
 				<button onClick={this.handleIngredientClick}>Add Ingredient</button>
-				{this.state.ingredients}
+				{this.state.ingredientForms}
 				<input type='submit'/>
 			</form>
 		)
