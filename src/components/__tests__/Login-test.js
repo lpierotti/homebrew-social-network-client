1da6c86257bfc307043ef53b17de6d9bdb5a9954
Luke Pierotti <lpierotti2@gmail.com>
2017-11-13 15:20:17 -0500

    fixed tests

--- src/components/__tests__/Login-test.js
+++ src/components/__tests__/Login-test.js
@@ -2,32 +2,33 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import Login from '../Login'
 
-describe('Login Component', function() {
+describe('Login Component', () => {
+
 	
-	it('should render without throwing an error', function() {
+	it('should render without throwing an error', () => {
 		expect(shallow(<Login />).exists(<form className='login'></form>)).toBe(true)
 	})
 
-	it('renders a email input', function() {
+	it('renders a email input', () => {
 		expect(shallow(<Login />).find('#email').length).toEqual(1)
 	})
 
-	it('renders a password input', function() {
+	it('renders a password input', () => {
 		expect(shallow(<Login />).find('#password').length).toEqual(1)
 	})
 	
-	describe('Email input', function() {
+	describe('Email input', () => {
 		
-		it('should respond change and change the state of the Login Component', function() {
+		it('should respond change and change the state of the Login Component', () => {
 			const wrapper = shallow(<Login />)
 			wrapper.find('#email').simulate('change', {target: {name: 'email', value: 'blah@gmail.com'}})
 			expect(wrapper.state('email')).toEqual('blah@gmail.com')
 		})
 	})
 	
-	describe('Password input', function() {
+	describe('Password input', () => {
 		
-		it('should respond change and change the state of the Login Component', function() {
+		it('should respond change and change the state of the Login Component', () => {
 			const wrapper = shallow(<Login />)
 			wrapper.find('#password').simulate('change', {target: {name: 'password', value: 'cats'}})
 			expect(wrapper.state('password')).toEqual('cats')
