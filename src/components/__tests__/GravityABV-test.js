986e6d1213cc89ccfe56d488c66e8314f6cd4b4f
Luke Pierotti <lpierotti2@gmail.com>
2017-11-26 17:58:30 -0500

    finished unit tests for GravityABV component

--- src/components/__tests__/GravityABV-test.js
+++ src/components/__tests__/GravityABV-test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { mount } from 'enzyme';
 import GravityABV from '../GravityABV';
 import renderer from 'react-test-renderer';
 
@@ -10,5 +10,24 @@ describe('GravityABV component', () => {
 			<GravityABV changeGravity={() => {}} data={{}}/>
 		).toJSON();
 		expect(tree).toMatchSnapshot();
+	})
+
+	const mockChangeGravityfn = jest.fn();
+	let wrapper;
+	
+	beforeAll(() => {
+		wrapper = mount(<GravityABV changeGravity={mockChangeGravityfn} data={{}}/>)
+	})
+
+	describe('input changes', () =>{
+
+		it('calls the changeGravity function when OG is changed', () => {
+			wrapper.find('input[name="og"]').simulate('change', {target: {name: 'og', value: '1.000'}})
+			expect(mockChangeGravityfn.mock.calls.length).toBe(1)
+		}) 
+		it('calls the changeGravity function when FG is changed', () => {
+			wrapper.find('input[name="fg"]').simulate('change', {target: {name: 'fg', value: '1.000'}})
+			expect(mockChangeGravityfn.mock.calls.length).toBe(2)
+		}) 
 	})
 })
